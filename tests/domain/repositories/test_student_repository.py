c0922d8b6c9f8c8c873482693b1764c04c14d31f
lorranyoliveira <lolo.souza80@yahoo.com>
2023-06-06 21:26:44 -0300

    tests #10 Adiciona testes do repository e da service

--- tests/domain/repositories/test_student_repository.py
+++ tests/domain/repositories/test_student_repository.py
@@ -1,224 +1,208 @@
-# from domain.models.student import Student
-# from infrastructure.repositories.student_repository import StudentRepository
-# from domain.models.student import Status
-# from fastapi import HTTPException, status
-# import pytest
-
-
-# # Test data
-# test_student_data = {
-#     "bairro": "Test Bairro",
-#     "cep": "12345678",
-#     "cidade": "Test City",
-#     "cpf": "17553041025",
-#     "data_nascimento": "2000-01-01",
-#     "deficiencia": False,
-#     "descricao_endereco": "Test Address",
-#     "email": "test@example.com",
-#     "login": "testuser",
-#     "nome": "Test User",
-#     "senha": "testpassword",
-#     "status": Status.PRODUCAO,
-#     "telefone": "12234567890"
-# }
-
-# # Mocking Session class for testing
-
-
-# class MockSession:
-#     def merge(self, student):
-#         pass
-
-#     def add(self, student):
-#         pass
-
-#     def commit(self):
-#         pass
-
-#     def query(self, model):
-#         return self
-
-#     def all(self):
-#         return []
-
-#     def filter(self, condition):
-#         return self
-
-#     def first(self):
-#         return None
-
-#     def delete(self, student):
-#         pass
-
-
-# def mock_db_session():
-#     return MockSession()
-
-
-# def test_save_student():
-#     student_repo = StudentRepository(mock_db_session)
-#     student = Student(**test_student_data)
-
-#     result = student_repo.save_student(student)
-
-#     assert result == student
-
-
-# def test_find_all_student():
-#     student_repo = StudentRepository(mock_db_session)
-
-#     result = student_repo.find_all_student()
-
-#     assert result == []
-
-
-# def test_find_by_login_student():
-#     student_repo = StudentRepository(mock_db_session)
-
-#     result = student_repo.find_by_login_student("testuser")
-
-#     assert result is None
-
-
-# def test_exists_by_cpf_student():
-#     student_repo = StudentRepository(mock_db_session)
-
-#     result = student_repo.exists_by_cpf_student("12345678901")
-
-#     assert result is False
-
-
-# def test_delete_by_login_student():
-#     student_repo = StudentRepository(mock_db_session)
-
-#     student_repo.delete_by_login_student("testuser")
-
-#     # No assertion, just making sure the function runs without errors
-
-
-# def test_update_by_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     student = Student(**test_student_data)
-
-#     with pytest.raises(HTTPException) as exc:
-#         student_repo.update_by_login(student)
-
-#     assert exc.value.status_code == status.HTTP_404_NOT_FOUND
-#     assert str(exc.value.detail) == "student não encontrado"
-
-
-# def test_save_student_existing_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     student = Student(**test_student_data)
-#     student.login = "existinguser"
-
-#     result = student_repo.save_student(student)
-
-#     assert result == student
-
-
-# def test_find_by_login_student_existing_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     existing_student = Student(**test_student_data)
-#     existing_student.login = "existinguser"
-#     mock_db_session().first = lambda: existing_student
-
-#     result = student_repo.find_by_login_student("existinguser")
-
-#     assert result == existing_student
-
-
-# def test_exists_by_cpf_student_existing_cpf():
-#     student_repo = StudentRepository(mock_db_session)
-#     existing_student = Student(**test_student_data)
-#     existing_student.cpf = "12345678901"
-#     mock_db_session.first = lambda: existing_student
-
-#     result = student_repo.exists_by_cpf_student("12345678901")
-
-#     assert result is True
-
-
-# def test_delete_by_login_student_existing_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     existing_student = Student(**test_student_data)
-#     existing_student.login = "existinguser"
-#     mock_db_session.first = lambda: existing_student
-
-#     student_repo.delete_by_login_student("existinguser")
-
-#     # No assertion, just making sure the function runs without errors
-
-
-# def test_update_by_login_existing_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     existing_student = Student(**test_student_data)
-#     existing_student.login = "existinguser"
-#     mock_db_session.first = lambda: existing_student
-
-#     result = student_repo.update_by_login(existing_student)
-
-#     assert result == existing_student
-
-
-# def test_save_student_without_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     student = Student(**test_student_data)
-#     student.login = None
-
-#     result = student_repo.save_student(student)
-
-#     assert result == student
-
-
-# def test_find_by_login_student_nonexistent_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     mock_db_session.first = lambda: None
-
-#     result = student_repo.find_by_login_student("nonexistentuser")
-
-#     assert result is None
-
-
-# def test_exists_by_cpf_student_nonexistent_cpf():
-#     student_repo = StudentRepository(mock_db_session)
-#     mock_db_session.first = lambda: None
-
-#     result = student_repo.exists_by_cpf_student("99999999999")
-
-#     assert result is False
-
-
-# def test_update_by_login_nonexistent_login():
-#     student_repo = StudentRepository(mock_db_session)
-#     mock_db_session.first = lambda: None
-
-#     with pytest.raises(HTTPException) as exc:
-#         student_repo.update_by_login(
-#             student_request=Student(**test_student_data))
-
-#     assert exc.value.status_code == status.HTTP_404_NOT_FOUND
-#     assert str(exc.value.detail) == "student não encontrado"
-
-
-# def test_validate_student_complete_fields():
-#     student_repo = StudentRepository(mock_db_session)
-#     student = Student(**test_student_data)
-
-#     result = student_repo.validate_student(student)
-
-#     assert result["completeStatus"] is True
-
-
-# def test_validate_student_incomplete_fields():
-#     student_repo = StudentRepository(mock_db_session)
-#     student_data = test_student_data.copy()
-#     student_data["nome"] = ""
-#     student_data["cpf"] = ""
-#     student = Student(**student_data)
-
-#     result = student_repo.validate_student(student)
-
-#     assert result["completeStatus"] is False
-#     assert result["nome"]["status"] is False
-#     assert result["cpf"]["status"] is False
+from unittest import mock
+from unittest.mock import MagicMock, patch
+import pytest
+from src.domain.models.student import Status, Student, StudentResponse
+from src.infrastructure.repositories.student_repository import StudentRepository
+from sqlalchemy.orm import Session
+from unittest.mock import MagicMock
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[3]))
+
+
+def generate_session():
+    return MagicMock(spec=Session)
+
+
+def test_find_all():
+    # Arrange
+    database = generate_session()
+    student_repository = StudentRepository(database)
+    expected_result = [Student(), Student()]
+    database().query().all.return_value = expected_result
+
+    # Act
+    result = student_repository.find_all_student()
+
+    # Assert
+    assert result == expected_result
+
+
+def test_save_new_student():
+    # Arrange
+    database = generate_session()
+    test_session = database
+
+    student = Student(
+        bairro= "Test Bairro",
+        cep= "12345678",
+        cidade= "Test City",
+        cpf= "17553041025",
+        data_nascimento= "2000-01-01",
+        deficiencia= False,
+        descricao_endereco= "Test Address",
+        email= "testamis@gmail.com",
+        login= "amisteste2",
+        nome= "amisteste1",
+        senha= "testpassword",
+        status= 1,
+        telefone= "12234567890"
+    )
+    test_session.query().filter().first.return_value = None
+    student_repository = StudentRepository(database)
+
+    # Act
+    result = student_repository.save_student(student)
+
+    # Assert"12234567890"
+    assert result == student
+
+    test_session.close()
+
+
+def test_validateStudent():
+    student = Student(
+        bairro= "Test Bairro",
+        cep= "12345678",
+        cidade= "Test City",
+        cpf= "17553041025",
+        data_nascimento= "2000-01-01",
+        deficiencia= False,
+        descricao_endereco= "Test Address",
+        email= "testamis@gmail.com",
+        login= "amisteste2",
+        nome= "amisteste1",
+        senha= "testpassword",
+        status= Status.PRODUCAO,
+        telefone= "12234567890"
+    )
+
+    database = generate_session()
+
+    result = StudentRepository(database).validate_student(student)
+    
+    assert result['bairro']['status'] is True
+    assert result['cep']['status'] is True
+    assert result['cidade']['status'] is True
+    assert result['cpf']['status'] is True
+    assert result['dNascimento']['status'] is True
+    assert result['email']['status'] is True
+    assert result['login']['status'] is True
+    assert result['nome']['status'] is True
+    assert result['senha']['status'] is True
+    assert result['status']['status'] is True
+    assert result['telefone']['status'] is True
+    assert result['completeStatus'] is True
+
+def test_invalidateStudent():
+   ## Arrange
+    student = Student(
+        bairro= "Test Bairro",
+        cep= "12345678",
+        cidade= "Test City",
+        cpf='07497550',  # cpf inválido
+        data_nascimento= "2000-01-01",
+        deficiencia= False,
+        descricao_endereco= "Test Address",
+        email= "testamis@gmail.com",
+        login= "amisteste2",
+        nome= "amisteste1",
+        senha= "testpassword",
+        status= Status.PRODUCAO,
+        telefone= "12234567890"
+    )
+
+    database = generate_session()
+    result = StudentRepository(database).validate_student(student)
+
+    assert result['bairro']['status'] is True
+    assert result['cep']['status'] is True
+    assert result['cidade']['status'] is True
+    assert result['cpf']['status'] is False
+    assert result['dNascimento']['status'] is True
+    assert result['email']['status'] is True
+    assert result['login']['status'] is True
+    assert result['nome']['status'] is True
+    assert result['senha']['status'] is True
+    assert result['status']['status'] is True
+    assert result['telefone']['status'] is True
+    assert result['completeStatus'] is False
+
+
+
+def test_invalidateStudent_2():
+  ##  Arrange
+    student = Student(
+        bairro= "Test Bairro",
+        cep= "12345678",
+        cidade= "Test City",
+        cpf= "17553041025",
+        data_nascimento= "2000-01-01",
+        deficiencia= False,
+        descricao_endereco= "Test Address",
+        email= "testamis@gmail.com",
+        login= "amisteste2",
+        nome= "amisteste1",
+        senha= "testpassword",
+        status= Status.PRODUCAO,
+        telefone= "112566"
+    )
+
+    database = generate_session()
+    result = StudentRepository(database).validate_student(student)
+
+    assert result['bairro']['status'] is True
+    assert result['cep']['status'] is True
+    assert result['cidade']['status'] is True
+    assert result['cpf']['status'] is True
+    assert result['dNascimento']['status'] is True
+    assert result['email']['status'] is True
+    assert result['login']['status'] is True
+    assert result['nome']['status'] is True
+    assert result['senha']['status'] is True
+    assert result['status']['status'] is True
+    assert result['telefone']['status'] is False
+    assert result['completeStatus'] is False
+
+@mock.patch("infrastructure.repositories.student_repository")
+def test_find_all(mock_repository):
+    # Criação do mock do repositório
+    mock_repository_instance = mock_repository.return_value
+    mock_repository_instance.find_all.return_value = [
+        StudentResponse(
+            bairro= "Test Bairro",
+            cep= "12345678",
+            cidade= "Test City",
+            cpf= "17553041025",
+            data_nascimento= "2000-01-01",
+            deficiencia= False,
+            descricao_endereco= "Test Address",
+            email= "testamis@gmail.com",
+            login= "amisteste2",
+            nome= "amisteste1",
+            senha= "testpassword",
+            status= Status.PRODUCAO,
+            telefone= "12234567890"
+        )
+    ]
+
+    students = mock_repository_instance.find_all()
+
+    mock_repository_instance.find_all.assert_called_once()
+
+    # Verifica o resultado retornado pela função
+    assert len(students) == 1
+    assert students[0].bairro == "Test Bairro"
+    assert students[0].cep == "12345678"
+    assert students[0].cidade == "Test City"
+    assert students[0].cpf == "17553041025"
+    assert students[0].data_nascimento == "2000-01-01"
+    assert students[0].deficiencia is False
+    assert students[0].descricao_endereco == "Test Address"
+    assert students[0].email == "testamis@gmail.com"
+    assert students[0].login == "amisteste2"
+    assert students[0].nome == "amisteste1"
+    assert students[0].senha == "testpassword"
+    assert students[0].status == Status.PRODUCAO
+    assert students[0].telefone == "12234567890"
